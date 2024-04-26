77f7e8f27dd227dd1e02736576d7c89519259ff0
NiceHim <103509667+NiceHim@users.noreply.github.com>
2024-04-26 23:35:16 +0800

    Add files via upload

---
+++ eval_models.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+cd benchmark
+python3 run.py resnet50 -d cpu -t eval --metrics model_flops,cpu_peak_mem --bs 32
+python3 run.py densenet121 -d cpu -t eval --metrics model_flops,cpu_peak_mem --bs 32
+python3 run.py mobilenet_v3_large -d cpu -t eval --metrics model_flops,cpu_peak_mem --bs 32
+python3 run.py vgg16 -d cpu -t eval --metrics model_flops,cpu_peak_mem --bs 32
+python3 run.py resnext50_32x4d -d cpu -t eval --metrics model_flops,cpu_peak_mem --bs 32
+python3 run.py BERT_pytorch -d cpu -t eval --metrics model_flops,cpu_peak_mem --bs 4
+python3 run.py hf_Longformer -d cpu -t eval --metrics model_flops,cpu_peak_mem --bs 4
+python3 run.py hf_DistilBert -d cpu -t eval --metrics model_flops,cpu_peak_mem --bs 4
+python3 run.py hf_Reformer -d cpu -t eval --metrics model_flops,cpu_peak_mem --bs 4
+python3 run.py hf_Albert -d cpu -t eval --metrics model_flops,cpu_peak_mem --bs 4
+python3 run.py pytorch_unet -d cpu -t eval --metrics model_flops,cpu_peak_mem
+python3 run.py detectron2_maskrcnn_r_50_fpn -d cpu -t eval --metrics model_flops,cpu_peak_mem
+python3 run.py detectron2_maskrcnn_r_50_c4 -d cpu -t eval --metrics model_flops,cpu_peak_mem
+python3 run.py yolov3 -d cpu -t eval --metrics model_flops,cpu_peak_mem --bs 8 
+python3 run.py vision_maskrcnn -d cpu -t eval --metrics model_flops,cpu_peak_mem
+python3 run.py detectron2_fasterrcnn_r_50_c4 -d cpu -t eval --metrics model_flops,cpu_peak_mem
+python3 run.py detectron2_fasterrcnn_r_50_dc5 -d cpu -t eval --metrics model_flops,cpu_peak_mem
+python3 run.py timm_vovnet -d cpu -t eval --metrics model_flops,cpu_peak_mem --bs 32 
+python3 run.py nvidia_deeprecommender -d cpu -t eval --metrics model_flops,cpu_peak_mem 
+python3 run.py dlrm -d cpu -t eval --metrics model_flops,cpu_peak_mem
+python3 run.py speech_transformer -d cpu -t eval --metrics model_flops,cpu_peak_mem
+python3 run.py demucs -d cpu -t eval --metrics model_flops,cpu_peak_mem
+python3 run.py tts_angular -d cpu -t eval --metrics model_flops,cpu_peak_mem
+python3 run.py hf_GPT2 -d cpu -t eval --metrics model_flops,cpu_peak_mem
+python3 run.py hf_GPT2_large -d cpu -t eval --metrics model_flops,cpu_peak_mem
+python3 run.py llama -d cpu -t eval --metrics model_flops,cpu_peak_mem
+python3 run.py llama_v2_7b_16h -d cpu -t eval --metrics model_flops,cpu_peak_mem
---
+++ install_models.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+apt-get update -y
+apt-get install libgl1 -y
+apt-get install -y python3 python3-pip
+python3 -m pip install --upgrade pip
+python3 -m pip install torch
+python3 -m pip install torchvision torchaudio torchtext
+python3 -m pip install numba
+export DNNL_DEFAULT_FPMATH_MODE=BF16
+export LRU_CACHE_CAPACITY=1024
+export HUGGING_FACE_HUB_TOKEN=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
+git clone https://github.com/pytorch/benchmark.git
+cd benchmark
+
+models=(
+	"resnet50" "densenet121" "mobilenet_v3_large" 
+	"vgg16" "resnext50_32x4d" "BERT_pytorch" 
+	"hf_Longformer" "hf_DistilBert" "hf_Reformer"
+	"hf_Albert" "pytorch_unet" "detectron2_maskrcnn_r_50_fpn"
+	"detectron2_maskrcnn_r_50_c4" "yolov3" "vision_maskrcnn"
+	"detectron2_fasterrcnn_r_50_c4" "detectron2_fasterrcnn_r_50_dc5" "timm_vovnet"
+	"nvidia_deeprecommender" "dlrm" "speech_transformer" "demucs"
+	"tts_angular" "hf_GPT2" "hf_GPT2_large"
+	"llama" "llama_v2_7b_16h"
+)	
+
+
+for model in "${models[@]}"; do
+	echo "Installing $model..."
+	python3 install.py $model
+done
+
+cd . .
+
+echo "All Machine Learning Models installed successfully."
